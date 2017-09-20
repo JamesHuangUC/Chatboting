cbd64082d2f6c3c0cea58fdfc954d3e9f580e479
James Huang <jameshuanguc@gmail.com>
2017-09-19 23:14:57 -0700

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chatboting
+
+This is a Facebook messenger chatbot. The chatbot currently is able to have a simple conversation with the user, and forcast weather. I hosted this on Heroku, and there will be more features adding to it.
+
+
+
+## Integerations
+
+[Facebook Messenger](https://developers.facebook.com/)
+[API.ai](https://console.api.ai)
+[Open Weather Map API](http://openweathermap.org/)
+
+
+
+## Source
+
+[Creating a Simple Facebook Messenger AI Bot with API.ai in Node.js](http://www.girliemac.com/blog/2017/01/06/facebook-apiai-bot-nodejs/)
---
+++ index.js
@@ -0,0 +1,243 @@
+/*jshint esversion: 6 */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiai = require('apiai');
+
+const token = process.env.FB_PAGE_ACCESS_TOKEN;
+const access = process.env.FB_ACCESS_TOKEN;
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
+const apiaiApp = apiai(APIAI_TOKEN);
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === token) {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('No entry');
+});
+
+// Receive Messages
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+    var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}
+
+function sendTextMessage(recipientId, messageText) {
+
+  // Handle API.AI
+  let apiaiHandle = apiaiApp.textRequest(messageText, {
+      sessionId: 'randomSessionId'
+  });
+
+  apiaiHandle.on('response', function(response) {
+    console.log(response);
+
+    let aiText = response.result.fulfillment.speech;
+
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: aiText
+      }
+    };
+
+    callSendAPI(messageData);
+  });
+
+  apiaiHandle.on('error', function(error) {
+    console.log(error);
+  });
+
+  apiaiHandle.end();
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: access },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'));
+});
+
+/* Webhook for API.ai to get response from the 3rd party API */
+app.post('/ai', (req, res) => {
+  console.log('*** Webhook for api.ai query ***');
+  console.log(req.body.result);
+
+  if (req.body.result.action === 'weather') {
+    console.log('*** weather ***');
+    let city = req.body.result.parameters['geo-city'];
+    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
+    restUrl = restUrl.replace(/ /g, "-");
+    console.log(`TEST ${restUrl}`);
+
+    request.get(restUrl, (err, response, body) => {
+      if (!err && response.statusCode == 200) {
+        let json = JSON.parse(body);
+        console.log(json);
+        let tempF = ~~(json.main.temp * 9/5 - 459.67);
+        let tempC = ~~(json.main.temp - 273.15);
+        let msg = 'The current condition in ' + json.name + ' is ' + json.weather[0].description + ' and the temperature is ' + tempF + ' ℉ (' +tempC+ ' ℃).';
+        return res.json({
+          speech: msg,
+          displayText: msg,
+          source: 'weather'
+        });
+      } else {
+        let errorMessage = 'I failed to look up the city name.';
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: errorMessage
+          }
+        });
+      }
+    });
+  }
+
+});
